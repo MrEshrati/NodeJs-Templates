dd13be8de44da97761ff634e621aa8aecb93e0e1
MrEshrati <arshiaeshrati1385@gmail.com>
2026-09-05 00:17:08 +0330

    feat(account): expose protected profile update endpoint

--- Without-Packages/routes/profile.route.js
+++ Without-Packages/routes/profile.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const profileController = require("../controllers/profile.controller");
 const requireAccessToken = require("../middlewares/accessAuth.middleware");
 const createRequestThrottle = require("../middlewares/requestThrottle.middleware");
+const validateRequest = require("../middlewares/validation.middleware");
+const validateProfileUpdate = require("../validators/profileUpdate.validator");
 
 const router = express.Router();
 
@@ -11,11 +13,25 @@ const profileRetrievalThrottle = createRequestThrottle({
   windowMs: 60 * 1000,
 });
 
+const profileModificationThrottle = createRequestThrottle({
+  scope: "profile-modification:v1",
+  maxRequests: 10,
+  windowMs: 60 * 1000,
+});
+
 router.get(
   "/",
   profileRetrievalThrottle,
   requireAccessToken,
   profileController.getProfile,
+);
+
+router.patch(
+  "/",
+  profileModificationThrottle,
+  requireAccessToken,
+  validateRequest(validateProfileUpdate),
+  profileController.updateProfile,
 );
 
 module.exports = router;
