a47390552c76c90e60c816a70786c333d5d142ec
MrEshrati <arshiaeshrati1385@gmail.com>
2026-09-04 23:02:50 +0330

    feat(account): mount profile retrieval endpoint

--- Without-Packages/app.js
+++ Without-Packages/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const authRouter = require("./routes/auth.route");
+const profileRouter = require("./routes/profile.route");
 const errorHandler = require("./middlewares/errorHandler.middleware");
 require("dotenv").config();
 
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
 
 app.use(errorHandler);
 
