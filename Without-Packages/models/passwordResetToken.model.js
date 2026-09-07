07adff90cb54f1568d35401b6ed24d4065c4a353
MrEshrati <arshiaeshrati1385@gmail.com>
2026-09-07 19:10:58 +0330

    feat(password): add password reset token model

---
+++ Without-Packages/models/passwordResetToken.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+      index: true,
+    },
+    tokenHash: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+      expires: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("PasswordResetToken", tokenSchema);
