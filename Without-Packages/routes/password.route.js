0b96d2dfde096f87957c1ebd0fbf2277ce8bf8ed
MrEshrati <arshiaeshrati1385@gmail.com>
2026-09-08 17:29:46 +0330

    feat(password-reset): wire reset confirmation endpoint

--- Without-Packages/routes/password.route.js
+++ Without-Packages/routes/password.route.js
@@ -3,6 +3,7 @@ const passwordResetController = require("../controllers/passwordReset.controller
 const createRequestThrottle = require("../middlewares/requestThrottle.middleware");
 const validateRequest = require("../middlewares/validation.middleware");
 const validateResetPassword = require("../validators/passwordResetRequest.validator");
+const validatePasswordResetConfirm = require("../validators/passwordResetConfirm.validator");
 
 const router = express.Router();
 
@@ -12,11 +13,24 @@ const passwordResetThrottle = createRequestThrottle({
   windowMs: 60 * 60 * 1000,
 });
 
+const passwordResetConfirmThrottle = createRequestThrottle({
+  scope: "password-reset-confirm:v1",
+  maxRequests: 10,
+  windowMs: 60 * 60 * 1000,
+});
+
 router.post(
   "/reset",
   passwordResetThrottle,
   validateRequest(validateResetPassword),
   passwordResetController.requestPasswordReset,
+);
+
+router.post(
+  "/reset/confirm",
+  passwordResetConfirmThrottle,
+  validateRequest(validatePasswordResetConfirm),
+  passwordResetController.confirmPasswordReset,
 );
 
 module.exports = router;
