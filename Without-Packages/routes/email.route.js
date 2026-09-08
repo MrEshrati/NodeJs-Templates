5668246ccffd6895fed2da8b18bdd30c168236c4
MrEshrati <arshiaeshrati1385@gmail.com>
2026-09-09 01:12:38 +0330

    feat(email): wire email change confirmation endpoint

--- Without-Packages/routes/email.route.js
+++ Without-Packages/routes/email.route.js
@@ -4,11 +4,18 @@ const createRequestThrottle = require("../middlewares/requestThrottle.middleware
 const requireAccessToken = require("../middlewares/accessAuth.middleware");
 const validateRequest = require("../middlewares/validation.middleware");
 const validateEmailChangeRequest = require("../validators/emailChangeRequest.validator");
+const validateEmailChangeConfirm = require("../validators/emailChangeConfirm.validator");
 
 const router = express.Router();
 
 const emailChangeRequestThrottle = createRequestThrottle({
   scope: "email-change-request:v1",
+  maxRequests: 10,
+  windowMs: 60 * 60 * 1000,
+});
+
+const emailChangeConfirmThrottle = createRequestThrottle({
+  scope: "email-change-confirm:v1",
   maxRequests: 10,
   windowMs: 60 * 60 * 1000,
 });
@@ -19,6 +26,13 @@ router.post(
   requireAccessToken,
   validateRequest(validateEmailChangeRequest),
   emailChangeController.requestEmailChange,
+);
+
+router.post(
+  "/change/confirm",
+  emailChangeConfirmThrottle,
+  validateRequest(validateEmailChangeConfirm),
+  emailChangeController.confirmEmailChange,
 );
 
 module.exports = router;
