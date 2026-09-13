ae31bf3fb492f413f9b311bd50255677148c7177
MrEshrati <arshiaeshrati1385@gmail.com>
2026-09-13 17:08:12 +0330

    feat(notifications): add notification feed route

---
+++ Without-Packages/routes/notification.route.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const notificationController = require("../controllers/notification.controller");
+const requireAccessToken = require("../middlewares/accessAuth.middleware");
+const createRequestThrottle = require("../middlewares/requestThrottle.middleware");
+const validateQuery = require("../middlewares/queryValidation.middleware");
+const validateNotificationList = require("../validators/notificationList.validator");
+
+const router = express.Router();
+
+const notificationThrottle = createRequestThrottle({
+  scope: "notifications:v1",
+  maxRequests: 120,
+  windowMs: 60 * 1000,
+});
+
+router.get(
+  "/",
+  notificationThrottle,
+  requireAccessToken,
+  validateQuery(validateNotificationList),
+  notificationController.listNotifications,
+);
+
+module.exports = router;
